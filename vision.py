8969afbcc5c068c3128b1d1c35f9a04bc9812170
Daniel Efres <da947619@ucf.edu>
2025-04-05 18:11:35 -0400

    added all requirements, gitignore and added base logic for cv

---
+++ vision.py
@@ -0,0 +1,23 @@
+from eyeGestures.utils import VideoCapture
+from eyeGestures import EyeGestures_v3
+
+
+gestures = EyeGestures_v3()
+cap = VideoCapture(0)
+calibrate = True
+screen_width = 500
+screen_height= 500
+
+
+while True:
+  ret, frame = cap.read()
+  event, cevent = gestures.step(frame,
+    calibrate,
+    screen_width,
+    screen_height,
+    context="my_context")
+
+  if event:
+    cursor_x, cursor_y = event.point[0], event.point[1]
+    fixation = event.fixation
+    saccades = event.saccadess
